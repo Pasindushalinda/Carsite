b0659507ea90e1dc5a30634d5d244bc1fa7168c5
Pasindushalinda <pshalinda5@gmail.com>
2023-10-11 12:13:39 +0530

    9-client side identity with NextAuth

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -2,15 +2,24 @@ import React from 'react'
 import { AiOutlineCar } from 'react-icons/ai'
 import Search from './Search'
 import Logo from './Logo'
+import LoginButton from './LoginButton'
+import UserActions from './UserActions'
+import { getCurrentUser } from '../actions/authActions'
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await getCurrentUser();
+
   return (
     <header className='
       sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
     '>
       <Logo />
       <Search />
-      <div>Login</div>
+      {user ? (
+        <UserActions user={user} />
+      ) : (
+        <LoginButton />
+      )}
     </header>
   )
 }
